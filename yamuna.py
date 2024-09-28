107e76b47b7d8732ab0965262afc622ae6e5b9ba
Yamunasri1825 <131263371+Yamunasri1825@users.noreply.github.com>
2024-09-28 17:20:56 +0530

    Initial commit

--- yamuna.py
+++ yamuna.py
@@ -4,7 +4,7 @@ def add_numbers(a, b):
 
 # Input from the user
 num1 = 40
-num2 = 20
+num2 = 40
 # Call the function and display the result
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {result}")
