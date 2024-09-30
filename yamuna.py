5928461cef98cef1ee2b5b5cebb3525f34deb6ec
Yamunasri1825 <131263371+Yamunasri1825@users.noreply.github.com>
2024-09-30 12:00:01 +0530

    i change number

--- yamuna.py
+++ yamuna.py
@@ -3,8 +3,8 @@ def add_numbers(a, b):
     return a + b
 
 # Input from the user
-num1 = 40
-num2 = 40
+num1 = 35
+num2 = 25
 # Call the function and display the result
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {result}")
