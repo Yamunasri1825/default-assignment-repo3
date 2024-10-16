651928f627cf654e8df1dfcb3834d6340c4fd229
Yamunasri1825 <131263371+Yamunasri1825@users.noreply.github.com>
2024-10-16 14:41:57 +0530

    added some files

--- yamuna.py
+++ yamuna.py
@@ -1,10 +1,1 @@
-# Function to add two numbers
-def add_numbers(a, b):
-    return a + b
-
-# Input from the user
-num1 = 35
-num2 = 25
-# Call the function and display the result
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is: {result}")
+print("hello world yamuna")
