a280331b963cefefc89aa7e7c02819626d2f5531
Yamunasri1825 <131263371+Yamunasri1825@users.noreply.github.com>
2024-11-03 12:38:17 +0530

    Initial commit

--- yamuna.py
+++ yamuna.py
@@ -1,10 +1,1 @@
-# Taking multiple inputs from the user
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-city = input("Enter your city: ")
-
-# Displaying the inputs
-print("\nUser Details:")
-print("Name:", name)
-print("Age:", age)
-print("City:", city)
+print("helllo")
